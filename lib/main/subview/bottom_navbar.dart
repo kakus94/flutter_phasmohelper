996d4e8f461a914667779a9125cba8a79d519b1c
kamil <kkarpiak94@gmail.com>
2024-09-30 10:40:22 +0200

    refactor code

--- lib/main/subview/bottom_navbar.dart
+++ lib/main/subview/bottom_navbar.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_phasmohelper/config/di.dart';
@@ -11,7 +13,7 @@ class BottomNavbar extends StatelessWidget {
 
   Color getColor(Evidence evidence) {
     if (getIt<MainCubit>().isEvidence(evidence)) {
-      return Colors.green;
+      return Colors.green.shade200;
     } else {
       return Colors.transparent;
     }
@@ -34,56 +36,47 @@ class BottomNavbar extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-
-                  Container(
-                      color: getColor(Evidence.emf5),
-                      child: itemNav("EMF5", 'assets/emf.png',
-                          onTap: () => changeEv(Evidence.emf5))),
-
-
-                  Container(
-                    color: getColor(Evidence.spiritBox),
-                    child: itemNav("Spiritbox", 'assets/spiritbox.png',
-                        onTap: () => getIt<MainCubit>()
-                            .changeEvidence(Evidence.spiritBox)),
+                  itemNav(
+                    "EMF5",
+                    'assets/emf.png',
+                    Evidence.emf5,
+                    onTap: () => changeEv(Evidence.emf5),
                   ),
-
-
-                  Container(
-                    color: getColor(Evidence.dots),
-                    child: itemNav("D.O.T.S", 'assets/dots.png',
-                        onTap: () =>
-                            getIt<MainCubit>().changeEvidence(Evidence.dots)),
+                  itemNav(
+                    "Spiritbox",
+                    'assets/spiritbox.png',
+                    Evidence.spiritBox,
+                    onTap: () => changeEv(Evidence.spiritBox),
                   ),
-
-
-                  Container(
-                    color: getColor(Evidence.freezing),
-                    child: itemNav("Freezing", 'assets/mrozne.png',
-                        onTap: () =>
-                            getIt<MainCubit>().changeEvidence(Evidence.freezing)),
+                  itemNav(
+                    "D.O.T.S",
+                    'assets/dots.png',
+                    Evidence.dots,
+                    onTap: () => changeEv(Evidence.dots),
                   ),
-
-
-                  Container(
-                    color: getColor(Evidence.orbs),
-                    child: itemNav("ORB", 'assets/orb.png',
-                        onTap: () =>
-                            getIt<MainCubit>().changeEvidence(Evidence.orbs)),
+                  itemNav(
+                    "Freezing",
+                    'assets/mrozne.png',
+                    Evidence.freezing,
+                    onTap: () => changeEv(Evidence.freezing),
                   ),
-                  
-                  Container(
-                    color: getColor(Evidence.ghostWriting),
-                    child: itemNav("Writing", 'assets/pismo.png',
-                        onTap: () => getIt<MainCubit>()
-                            .changeEvidence(Evidence.ghostWriting)),
+                  itemNav(
+                    "ORB",
+                    'assets/orb.png',
+                    Evidence.orbs,
+                    onTap: () => changeEv(Evidence.orbs),
                   ),
-
-                  Container(
-                    color: getColor(Evidence.ultrafilet),
-                    child: itemNav("ultrafiolent", 'assets/ultrafiolet.png',
-                        onTap: () => getIt<MainCubit>()
-                            .changeEvidence(Evidence.ultrafilet)),
+                  itemNav(
+                    "Writing",
+                    'assets/pismo.png',
+                    Evidence.ghostWriting,
+                    onTap: () => changeEv(Evidence.ghostWriting),
+                  ),
+                  itemNav(
+                    "ultrafiolent",
+                    'assets/ultrafiolet.png',
+                    Evidence.ultrafilet,
+                    onTap: () => changeEv(Evidence.ultrafilet),
                   ),
                 ],
               ),
@@ -92,18 +85,29 @@ class BottomNavbar extends StatelessWidget {
     );
   }
 
-  GestureDetector itemNav(String title, String icon,
+  SizedBox itemNav(String title, String icon, Evidence evidence,
       {required Function onTap}) {
-    return GestureDetector(
-      onTap: () => onTap(),
-      child: Column(
-        children: [
-          Image.asset(icon),
-          Text(
-            title,
-            style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+    return SizedBox(
+      width: 50,
+      height: 75,
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+        decoration: BoxDecoration(
+          color: getColor(evidence),
+          borderRadius: BorderRadius.circular(10),
+        ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(icon),
+              Text(
+                title,
+                style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
